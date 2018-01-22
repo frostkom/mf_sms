a0dbc058b95701fc0d61ae18a21079b21f55df67
frostkom <martin.fors@frostkom.se>
2018-01-22 14:38:32 +0100

    Add licence

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Plugin Name: MF SMS
 Plugin URI: https://github.com/frostkom/mf_sms
 Description: 
-Version: 2.3.15
+Version: 2.3.17
+Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_sms
