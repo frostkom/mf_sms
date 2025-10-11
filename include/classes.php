dbb2fd17388103b30ea88235de7200123eb3c0a5
frostkom <martin.fors@frostkom.se>
2025-10-11 20:00:14 +0200

    Remove old code

--- include/classes.php
+++ include/classes.php
@@ -657,19 +657,6 @@ class mf_sms
 		return $new_value;
 	}
 
-	/*function pre_update_option($new_value, $old_value)
-	{
-		$out = "";
-
-		if($new_value != '')
-		{
-			$obj_encryption = new mf_encryption(__CLASS__);
-			$out = $obj_encryption->encrypt($new_value, md5(AUTH_KEY));
-		}
-
-		return $out;
-	}*/
-
 	function settings_sms_callback()
 	{
 		$setting_key = get_setting_key(__FUNCTION__);
